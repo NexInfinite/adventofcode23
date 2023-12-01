510d55effa1536c506b9b4ece3d02a5dc941f775
Julian <julianjones663@gmail.com>
2023-12-01 07:54:08 +0000

    uh oh fucky wucky fixed

--- day1/c1/main.py
+++ day1/c1/main.py
@@ -1,1 +1,1 @@
-print(sum([int([x for x in line.rstrip() if x.isdigit()][0] + [x for x in line.rstrip() if x.isdigit()][0]) for line in open("words.txt", "r")]))
+print(sum([int([x for x in line.rstrip() if x.isdigit()][0] + [x for x in line.rstrip() if x.isdigit()][-1]) for line in open("words.txt", "r")]))
