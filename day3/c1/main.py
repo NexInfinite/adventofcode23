e0176bb870f0f19d4e7f8daee39bf115ae471e42
Julian Jones <julianjones663@gmail.com>
2023-12-04 00:15:39 +0000

    HAHAH LOLL!

--- day3/c1/main.py
+++ day3/c1/main.py
@@ -4,58 +4,57 @@ symbols = list(string.punctuation)
 symbols.remove(".")
 x_max = len(schematics[0]) - 1
 y_max = len(schematics) - 1
-
-def get_3x3(x, y):
-    arr = []
-    tets_index = [-1, 0, 1]
-    for y_change in tets_index:
-        y_i = y + y_change
-        for x_change in tets_index:
-            x_i = x + x_change
-            if (x_i >= 0 and x_i <= x_max) and (y_i >= 0 and y_i <= y_max):
-                arr.append(schematics[y_i][x_i])
-    return arr
-
-def check_digit(x, y):
-    if schematics[y][x].isdigit():
-        adjacents = get_3x3(x, y)
-        for value in adjacents:
-            if value in symbols:
-                return get_number(x, y)
-    return 0
                 
 def get_number(x, y):
+    global schematics
     total = []
     start = x
 
     # Go back till not digit
-    for i in range(x, 0, -1):
+    for i in range(x, -1, -1):
         if not schematics[y][i].isdigit():
             start = i + 1
             break
+        if i == 0:
+            start = 0
 
+    print(start)
     # count forward till not digit
     for i in range(start, x_max):
         if schematics[y][i].isdigit():
             total.append(schematics[y][i])
+            schematics[y][i] = "."
         else:
             return int("".join(total))
     return 0 if total == [] else int("".join(total))
 
+def gear_scan(x, y):
+    numbers = []
+    y_diffs = [-1, 0, 1]
+    for y_diff in y_diffs:
+        if 0 <= y + y_diff <= y_max:
+            new_y = y + y_diff
+            if schematics[new_y][x].isdigit():
+                numbers.append(get_number(x, new_y))
+            if x - 1 >= 0 and schematics[new_y][x - 1].isdigit():
+                numbers.append(get_number(x - 1, new_y))
+            if x + 1 <= x_max and schematics[new_y][x + 1].isdigit():
+                numbers.append(get_number(x + 1, new_y))
+    print(numbers)
+    return sum(numbers)
+
 def run():
     total = 0
     for y in range(len(schematics)):
-        skip = 0
         for x in range(len(schematics[y])):
-            if skip > 0:
-                skip -= 1
-            else:
-                dig = check_digit(x, y)
-                if dig > 0:
-                    total += dig
-                    print(dig, len(str(dig)))
-                    skip = len(str(dig))
-                
+            if not schematics[y][x].isdigit() and schematics[y][x] != ".":
+                total += gear_scan(x, y)
     print(total)
 
-run()
+if __name__ == "__main__":
+    # for x in range(len(schematics[1])):
+    #     if schematics[1][x] in symbols:
+    #         gear_scan(x, 1)
+    run()
+    # for schema in schematics:
+    #     print("".join(schema))
