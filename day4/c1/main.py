b75c90cd7c19b8db655215af9e5aca3635181c49
Julian Jones <julianjones663@gmail.com>
2023-12-05 03:10:49 +0000

    optimised challenge 2

--- day4/c1/main.py
+++ day4/c1/main.py
@@ -1,17 +1,8 @@
-cards = []
-
-with open("input.txt") as f:
-    for line in f.readlines():
-        line = line.rstrip().split(":")[1]
-        winning = [int(x) for x in line.split("|")[0].split(" ") if x != ""]
-        answers = [int(x) for x in line.split("|")[1].split(" ") if x != ""]
-        cards.append([winning, answers])
+cards = [[[int(x) for x in line.rstrip().split(":")[1].split("|")[0].split(" ") if x != ""], [int(x) for x in line.rstrip().split(":")[1] .split("|")[1].split(" ") if x != ""]] for line in open("test.txt")]
 
 total = 0
 for card in cards:
     card_score = 0
-    for answer in card[0]:
-        if answer in card[1]:
-            card_score = 1 if card_score == 0 else card_score + card_score
+    [(card_score := 1 if card_score == 0 else card_score*2) for answer in card[0] if answer in card[1]]
     total += card_score
 print(total)
